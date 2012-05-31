48228f0429384a502975bb58ed6cb8c23f369526
armin.miisc <armin.misic@stu.ssst.edu.ba>
2012-05-31 17:13:02 +0200

    Welcome

---
+++ src/snake2/Snake.java
@@ -0,0 +1,25 @@
+package snake2;
+
+import javax.swing.JFrame;
+
+
+public class Snake extends JFrame {
+
+    public Snake() {
+
+        add(new Board());
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(320, 340);
+        setLocationRelativeTo(null);
+        setTitle("Snake");
+
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Snake();
+    }
+}
+
